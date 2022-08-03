dbfcd928e213feac4d18904e407ecc255f42b562
jcrowe91 <jcrowe9119@gmail.com>
2022-08-02 20:51:34 -0500

    Added Windows Form for UI

---
+++ TheStoicRobotUI/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace TheStoicRobotUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tweetBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 265);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(293, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Would you like to tweet this quote?\r\n";
+            // 
+            // tweetBtn
+            // 
+            this.tweetBtn.Location = new System.Drawing.Point(187, 305);
+            this.tweetBtn.Name = "tweetBtn";
+            this.tweetBtn.Size = new System.Drawing.Size(293, 79);
+            this.tweetBtn.TabIndex = 1;
+            this.tweetBtn.Text = "Tweet!";
+            this.tweetBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(745, 514);
+            this.Controls.Add(this.tweetBtn);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "TheStoicRobot";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button tweetBtn;
+    }
+}
